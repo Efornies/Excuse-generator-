7aea1dabc45cf6e931feb9f35f48b68be3ab6255
Efornies <eleforbel@gmail.com>
2022-03-12 10:45:57 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("titulo").innerHTML = excusegenerator();
+};
+
+const excusegenerator = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let i = Math.floor(Math.random() * who.length);
+  let j = Math.floor(Math.random() * action.length);
+  let k = Math.floor(Math.random() * what.length);
+  let l = Math.floor(Math.random() * when.length);
+
+  return who[i] + " " + action[j] + " " + what[k] + " " + when[l];
 };
